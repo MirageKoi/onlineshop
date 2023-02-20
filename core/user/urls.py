3d3002e438577158c71cbbabd2ec30e1f3879baf
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-02-20 14:45:00 +0200

    improve auth

--- core/user/urls.py
+++ core/user/urls.py
@@ -8,5 +8,5 @@ app_name = "user"
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("signin/", views.SignInView.as_view(), name="signin"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(next_page="homepage"), name="logout")
 ]
