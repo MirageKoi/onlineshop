af027c7aaad4d88f8efc462a4818829c7bd6d5ab
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-02-21 14:49:12 +0200

    added storeupdateview

--- core/store/urls.py
+++ core/store/urls.py
@@ -6,4 +6,5 @@ app_name = "store"
 urlpatterns = [
     path("", views.StoreListView.as_view(), name="store"),
     path("create/", views.StoreCreateView.as_view(), name="create"),
+    path("<slug:slug>/edit", views.StoreUpdateView.as_view(), name="update"),
 ]
--- core/store/views.py
+++ core/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Product
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .forms import CreateProduct
 from django.urls import reverse_lazy
 
@@ -19,9 +19,17 @@ class StoreListView(ListView):
 
 
 class StoreCreateView(CreateView, SuperUserCheck):
-    # model = Product
+    model = Product
     form_class = CreateProduct
     template_name = "storecreate.html"
     success_url = reverse_lazy("store:store")
+
+
+class StoreUpdateView(UpdateView, SuperUserCheck):
+    model = Product
+    form_class = CreateProduct
+    template_name = "storecreate.html"
+    success_url = reverse_lazy("store:store")
+
 
 
